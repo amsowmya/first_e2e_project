a924ec6a777a95c214eac691d09def6b9660371c
amsowmya <soumya.pdit@gmail.com>
2023-11-12 20:52:28 +0530

    Added setup.py file

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 bash <file_name>.sh
 ```
 
+```
+python setup.py install
+```
+
+# another way you can mention -e . in your requirements file and you can run
+
+```
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name='DimondPricePrediction',
+    version='0.0.1',
+    author='Sowmya AM',
+    author_email='sowmya.anekonda@gmail.com',
+    packages=find_packages()
+)
