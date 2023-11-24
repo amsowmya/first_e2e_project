f443bdfe4f0ebc8748fafd22458f14db45943d68
amsowmya <soumya.pdit@gmail.com>
2023-11-25 00:00:01 +0530

    updated app.py

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ def predict_datapoint():
         return render_template("result.html", final_result=result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host='0.0.0.0', port=8080)
